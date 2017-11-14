80c6aa09cf5175d96fa598c6bf494bbf0efddfef
Stephen Williams <stephen@wearetmbr.com>
2017-11-13 18:11:51 -0600

    initial wordpress install

---
+++ wp-content/themes/aca/assets/scripts/main.js
@@ -0,0 +1,82 @@
+(function ($) {
+
+  // WOW ANIMATIONS
+
+  new WOW().init();
+
+	// ADD CLASS TO NAV ON SCROLL
+
+	$(window).scroll(function() {
+		if (($(window).scrollTop() > 70 && $(window).width() > 992 )){
+	 		$('.navbar').addClass('scrolled');
+	 	}
+	 	else {
+		 	$('.navbar').removeClass('scrolled');
+	 	}
+
+	}); // window.scroll
+
+  $('p:empty').remove();
+
+  // HERO SLIDER
+
+    $('#hero-slider').flexslider({
+      animation: "fade",
+      slideshow: true, // auto play on load
+      slideshowSpeed: 5000,
+      animationSpeed: 800,
+      pauseOnHover: true,
+      directionNav: true, //Boolean: Create navigation for previous/next navigation? (true/false)
+      controlNav: true, //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
+      prevText: "",
+      nextText: ""
+    });
+
+
+  // PRELOADER
+
+	// $(window).load(function(){
+  //   $('#preloader').fadeOut('slow',function(){$(this).remove();});
+	// });
+
+  // ISOTOPE GRID
+
+  $(window).load(function(){
+
+    // init Isotope
+    var $grid = $('.project-grid').isotope({
+      itemSelector: '.project-item',
+      layoutMode: 'masonry'
+    });
+    // filter functions
+    var filterFns = {
+      // show if number is greater than 50
+      numberGreaterThan50: function() {
+        var number = $(this).find('.number').text();
+        return parseInt( number, 10 ) > 50;
+      },
+      // show if name ends with -ium
+      ium: function() {
+        var name = $(this).find('.name').text();
+        return name.match( /ium$/ );
+      }
+    };
+    // bind filter button click
+    $('.filters-button-group').on( 'click', 'a', function() {
+      var filterValue = $( this ).attr('data-filter');
+      // use filterFn if matches value
+      filterValue = filterFns[ filterValue ] || filterValue;
+      $grid.isotope({ filter: filterValue });
+    });
+    // change is-checked class on buttons
+    $('.button-group').each( function( i, buttonGroup ) {
+      var $buttonGroup = $( buttonGroup );
+      $buttonGroup.on( 'click', 'a', function() {
+        $buttonGroup.find('.active').removeClass('active');
+        $( this ).addClass('active');
+      });
+    });
+  });
+
+
+})(jQuery);
---
+++ wp-content/themes/aca/front-page.php
@@ -0,0 +1,17 @@
+<?php get_header();
+
+// HOME PAGE TEMPLATE
+
+?>
+
+<div class="full-width-holder wow fadeIn" style="background-image: url('/wp-content/uploads/2017/10/place-holder-01.jpg');">
+	<div class="holder-content wow fadeIn" data-wow-duration="1s" data-wow-delay="1s">
+		<img src="/wp-content/uploads/2017/10/full-brand-02.png" alt="Stephen Williams Photography">
+		<p>I am currently revamping my photography site.<br> Please check back for updates</p>
+		<a href="" target="_blank"><i class=" fa fa-fw fa-instagram"></i></a>
+		<a href="mailto:stephenwillidesigns@gmail.com"><i class=" fa fa-fw fa-envelope"></i></a>
+	</div>
+</div>
+
+
+<?php get_footer();?>
